07dce1bf53eb6500745231948b1045c0d6072196
aseemrk <114413493+aseemrk@users.noreply.github.com>
2026-02-21 20:59:17 +0000

    Deploying to gh-pages from @ aseemrk/aseemrk.github.io@3684683268c99da7ed794f7711a6bb07cc211f41 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-joined-iscr-as-a-mopga-early-career-research-fellow-to-work-on-a-project-stabilizing-halide-perovskites-for-efficient-and-durable-solar-cells-more-details",
+          title: 'I joined ISCR as a MOPGA early career research fellow to work on...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
