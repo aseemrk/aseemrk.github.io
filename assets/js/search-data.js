49d552b5752ec360652f259ea0bd7558d0bdaa56
aseemrk <114413493+aseemrk@users.noreply.github.com>
2026-02-22 08:07:36 +0000

    Deploying to gh-pages from @ aseemrk/aseemrk.github.io@91497a5630b9753aab0e43a89d817612c4525e3f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -499,9 +499,9 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
-          description: "",
+            },},{id: "teachings-initiation-to-reserach-energy-in-chemistry",
+          title: 'Initiation to Reserach --  Energy in Chemistry',
+          description: "This course is designed for first year undergraduates, as a part of highly selective [CPES]https://formations.univ-rennes.fr/parcours/cycle-pluridisciplinaire-detudes-superieures-cpes-sciences-environnement-societe-sens) preparatory training.",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/data-science-fundamentals/";
             },},{id: "teachings-introduction-to-machine-learning",
